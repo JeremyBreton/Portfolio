024ec6ccf310ab93b4941ada0c889f93e1ccdc3b
Jérémy <breton.jeremy@live.fr>
2024-02-12 18:21:23 +0100

    init project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,20 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jérémy Breton</h1>
       </header>
     </div>
   );
