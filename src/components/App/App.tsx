4f18c5490329492e1308f12facc7a5ae4d2b3eda
Jérémy <breton.jeremy@live.fr>
2024-02-14 08:25:58 +0100

    add animation keyframe img

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,9 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={memoji5} alt="Jérémy Breton, développeur web fullstack" />
-        <h1>Jérémy Breton</h1>
-        <h2>Développeur Web Fullstack </h2>
+        <img
+          src={memoji5}
+          alt="Jérémy Breton, développeur web fullstack"
+          className="imgScale"
+        />
+        <div>
+          <h1>Jérémy Breton</h1>
+          <h2>Développeur Web Fullstack </h2>
+        </div>
       </header>
     </div>
   );
